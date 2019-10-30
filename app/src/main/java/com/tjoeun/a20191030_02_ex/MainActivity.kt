9c0fc08fbc073a5e4fb02b560230d8742db5784e
sjsid79 <wjdtlr0723>
2019-10-30 22:20:17 +0900

    토스트 예제

--- app/src/main/java/com/tjoeun/a20191030_02_ex/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191030_02_ex/MainActivity.kt
@@ -2,11 +2,21 @@ package com.tjoeun.a20191030_02_ex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+            var inputid = idEdt.text.toString()
+            var inputPw = pwEdt.text.toString()
+
+            Toast.makeText(this, String.format("ID : %s, PW : %s", inputid, inputPw), Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
